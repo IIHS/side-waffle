2f67347dab12765e735590896b2e0d34cf4862f9
pedwards <pedwards@iihs.org>
2016-01-08 15:09:02 -0500

    Adding element and dialog templates Aurelia using typescript

---
+++ TemplatePack/ItemTemplates/Web/JavaScript/Aurelia/TypescriptCustomElement/element.ts
@@ -0,0 +1,16 @@
+﻿import {bindable, bindingMode} from 'aurelia-framework';
+
+export class $safeitemname$ {
+
+    @bindable({
+        attribute: "text",
+        defaultBindingMode: bindingMode.oneWay
+    }) text: string = "";
+
+    constructor() {
+    }
+
+    activate() {
+    }
+
+}
---
+++ TemplatePack/ItemTemplates/Web/JavaScript/Aurelia/TypescriptDialogView/view.ts
@@ -0,0 +1,16 @@
+﻿import {inject} from "aurelia-framework";
+import {DialogController} from 'aurelia-dialog'
+
+@inject(DialogController)
+export class $safeitemname$ {
+
+    dialogController: DialogController;
+
+    constructor(dialogController: DialogController) {
+        this.dialogController = dialogController;
+    }
+
+    activate() {
+    }
+
+}
